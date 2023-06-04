2680ac42d6e69f1f3952bd2eeb3cef3529685d50
Jhojan <jhojan506@gmail.com>
2023-06-04 17:54:43 -0500

    prueba normal

--- app.js
+++ app.js
@@ -25,7 +25,16 @@
 
 // server()
 
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`hola mundo ${PORT}`))
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('¡Hola desde el backend!');
+});
+
+app.listen(5000, () => {
+  console.log('Servidor iniciado en el puerto 5000');
+});
