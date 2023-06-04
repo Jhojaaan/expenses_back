a8b8528df914b4f594af10fd917988ee8f0708c6
Jhojan <jhojan506@gmail.com>
2023-06-04 18:39:12 -0500

    nuevo app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const cors = require("cors");
-const { db } = require("./db/db");
-const { readdirSync } = require("fs");
+const express = require('express');
+const cors = require('cors');
+const { db } = require('./db/db');
+const { readdirSync } = require('fs');
 const app = express();
 
-require("dotenv").config();
+require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,22 +13,13 @@ app.use(express.json());
 app.use(cors());
 
 //routes
-readdirSync("./routes").map((route) =>
-    app.use("/api/v1", require("./routes/" + route))
-);
-
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)));
 
 const server = () => {
-    // db();
-    app.listen(PORT, () => {
-        console.log("listening to port:", PORT);
-    });
+  db();
+  app.listen(PORT, () => {
+    console.log('listening to port:', PORT);
+  });
 };
 
-app.get("/", (req, res) => {
-    app.listen(PORT, () => {
-        console.log("listening to port:", PORT);
-    });
-    // res.send('¡Hola desde el backend!');
-    // server();
-});
+server();
