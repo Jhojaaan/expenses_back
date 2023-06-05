9fd198f9044c46e8a02b2445b265b2f30844d835
Jhojan <jhojan506@gmail.com>
2023-06-04 19:19:24 -0500

    entra bd

--- db/db.js
+++ db/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const db = async () => {
+    console.log('Entra a la bd')
     try {
         mongoose.set('strictQuery', false)
         await mongoose.connect(process.env.MONGO_URL)
